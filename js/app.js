0ec6244d98717d94a72d0f703302060789f7ff32
SebastianAlen2 <sebastian.alen98@gmail.com>
2022-06-05 23:28:59 -0300

    Primer commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+let marca = Number(prompt('¿De donde es su marca? (America=1, Asia=2 o Europa=3'));
+let añoAuto = Number(prompt('¿De que año es su auto?'));
+let cantidad;
+let total;
+let base = 2000;
+
+function año(){
+    if((2022 - añoAuto) >= 20){
+        total = cantidad * 2
+    }else if ((2022 - añoAuto) < 20){
+        total = cantidad +10
+    }
+}
+
+
+if(marca == 0){
+    while(marca==0){marca = Number(prompt('Debe seleccionar la region (America=1, Asia=2 o Europa=3'))}
+}else if (añoAuto == 0){
+    while(añoAuto==0){añoAuto = Number(prompt('Escriba el año'))}
+}
+
+switch (marca) {
+    case 1:
+        cantidad = base * 1.15
+        año()
+        break;
+    case 2:
+        cantidad = base * 1.30
+        año()
+        break;
+    case 3:
+        cantidad = base * 1.50
+        año()
+    break;
+    
+    default:
+        break;
+}
+
+alert(total);
